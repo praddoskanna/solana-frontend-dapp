502256f6770ab1aab49c50d48532b61d80b52ab1
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2023-04-05 16:53:18 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,58 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
+import type { NextPage } from "next"
+import NavBar from '@/components/NavBar'
+import Disconnected from '@/components/Disconnected'
+import { useWallet } from "@solana/wallet-adapter-react"
+import Connected from "../components/Connected"
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+  const { connected } = useWallet()
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name="The NFT Collection for Buildoors" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box
+        w="full"
+        h="calc(100vh)"
+        bgImage={"url(/home-background.svg)"}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="calc(100vh)" justify="center">
+					{ /* NavBar */ }
+          <NavBar />
+          <Spacer />
+          <Center>
+            {connected ? 
+            <Connected />  :
+            <Disconnected />}
+						{ /* If connected, the second view, otherwise the first */ }
+          </Center>
+          <Spacer />
+
+          <Center>
+            <Box marginBottom={4} color="white">
+              <a
+                href="https://twitter.com/_buildspace"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                built with @_buildspace
+              </a>
+            </Box>
+          </Center>
+        </Stack>
+      </Box>
+    </div>
+  )
+}
